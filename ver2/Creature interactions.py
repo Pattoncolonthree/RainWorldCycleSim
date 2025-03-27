a53deee7d05d721df044b81053458bf015b0bde0
Pattoncolonthree <22182@fraser.school.nz>
2025-03-27 13:11:41 +1300

    update
    added reputation(TV) system for the aggression levels which will be added later.

--- ver2/Creature interactions.py
+++ ver2/Creature interactions.py
@@ -1,6 +1,20 @@
+import time
+aggression = 1
+has_spear = True
+has_rock = True
+
+def reputation(aggression):
+    if has_spear == True:
+        aggression += 2
+    if has_rock == True:
+        aggression += 1
+
 def scavenger(scav):
    
     print("You look towards the creature in front of you, it looks back at you, it's quills spiking up.")
+    time.sleep(2)
+
+    
 
 
 
