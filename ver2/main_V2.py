86194d643e27b4eb107fd30269f996af202835ba
Pattoncolonthree <22182@fraser.school.nz>
2025-06-26 13:50:09 +1200

    updaye
    test trial

--- ver2/main_V2.py
+++ ver2/main_V2.py
@@ -7,16 +7,7 @@ from PIL import Image, ImageTk
 from itertools import count
 import winsound
 import GUI_Creature_Interactions as ci
-import Starting_Room as R1
 
-class App(tk.Tk):
-   def __init__(self):
-      super().__init__()
-app = App()
-
-
-R1.room1()
-
-ci.Scavgui()
+ci.scav_gui()
 
 app.mainloop()
